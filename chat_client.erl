b0ff47fb4087945ddd77469d3d432202d105a243
AnunakGor <anunak.r@greyorange.com>
2025-02-03 22:51:58 +0530

    Resolved multiple client on same terminal problem

--- chat_client.erl
+++ chat_client.erl
@@ -1,9 +1,17 @@
--module(chat_client).
-
--export([start/1, send/2, send_private/3, list_clients/0, disconnect/1, loop/1]).
+-module(chat_client).                    
+  
+-export([start/1, send/1, send_private/2, list_clients/0, disconnect/0, loop/1]).
 
 start(Username) ->
-    spawn(fun() -> init_client(Username) end).
+    case whereis(chat_client) of
+        undefined ->
+            Pid = spawn(fun() -> init_client(Username) end),
+            register(chat_client, Pid),
+            {ok, Pid};
+        _ ->
+            io:format("A chat client is already running on this node. Cannot start another.~n"),
+            {error, already_running}
+    end.
 
 init_client(Username) ->
     Self = self(),
@@ -15,19 +23,40 @@ init_client(Username) ->
             io:format("Failed to connect as ~s: ~p~n", [Username, Reason])
     end.
 
-send(Username, Message) ->
-    chat_server:send_message(Username, Message).
+send(Message) ->
+    case whereis(chat_client) of
+        undefined ->
+            io:format("No chat client running on this node. Please start one first.~n"),
+            {error, not_started};
+        _ ->
+            chat_client ! {send, Message},
+            ok
+    end.
 
-send_private(From, To, Message) ->
-    chat_server:private_message(From, To, Message).
+send_private(To, Message) ->
+    case whereis(chat_client) of
+        undefined ->
+            io:format("No chat client running on this node. Please start one first.~n"),
+            {error, not_started};
+        _ ->
+            chat_client ! {send_private, To, Message},
+            ok
+    end.
 
 list_clients() ->
     Clients = chat_server:list_clients(),
     io:format("Connected clients: ~p~n", [Clients]),
     Clients.
 
-disconnect(Username) ->
-    chat_server:disconnect(Username).
+disconnect() ->
+    case whereis(chat_client) of
+        undefined ->
+            io:format("No chat client running on this node.~n"),
+            {error, not_started};
+        _ ->
+            chat_client ! disconnect,
+            ok
+    end.
 
 loop(Username) ->
     receive
@@ -52,6 +81,17 @@ loop(Username) ->
         {error, Reason} ->
             io:format("Error: ~p~n", [Reason]),
             loop(Username);
+        {send, Message} ->
+            chat_server:send_message(Username, Message),
+            loop(Username);
+        {send_private, To, Message} ->
+            chat_server:private_message(Username, To, Message),
+            loop(Username);
+        disconnect ->
+            chat_server:disconnect(Username),
+            io:format("Disconnected from chat server.~n"),
+            unregister(chat_client),
+            ok;
         Other ->
             io:format("Unknown message: ~p~n", [Other]),
             loop(Username)
