d63727ac0b4030e8f0b575c45952c13ae9599894
AnunakGor <anunak.r@greyorange.com>
2025-02-06 23:55:08 +0530

    Updated client code

--- chat_client.erl
+++ chat_client.erl
@@ -1,7 +1,8 @@
--module(chat_client).                    
-  
--export([start/1, send/1, send_private/2, list_clients/0, disconnect/0, loop/1, get_server_node/1]).
+-module(chat_client).
 
+-export([start/1, send/1, send_private/2, list_clients/0, disconnect/0, loop/1,
+         get_server_node/1,
+         set_topic/1, kick/1, mute/2, unmute/1, promote/1, get_topic/0, get_admins/0]).
 start(Username) ->
     case whereis(chat_client) of
         undefined ->
@@ -33,7 +34,8 @@ init_client(Username) ->
             io:format("Connected as ~s.~nPrevious chat history: ~p~n", [Username, History]),
             loop(Username);
         {error, Reason} ->
-            io:format("Failed to connect as ~s: ~p~n", [Username, Reason])
+            io:format("Failed to connect as ~s: ~p~n", [Username, Reason]),
+            loop(Username)
     end.
 
 send(Message) ->
@@ -71,10 +73,46 @@ disconnect() ->
             ok
     end.
 
+set_topic(NewTopic) ->
+    chat_client ! {set_topic, NewTopic},
+    ok.
+
+kick(Target) ->
+    chat_client ! {kick, Target},
+    ok.
+
+mute(Target, Duration) ->
+    chat_client ! {mute, Target, Duration},
+    ok.
+
+unmute(Target) ->
+    chat_client ! {unmute, Target},
+    ok.
+
+promote(Target) ->
+    chat_client ! {promote, Target},
+    ok.
+
+get_topic() ->
+    Topic = chat_server:get_topic(),
+    io:format("Current topic: ~s~n", [Topic]),
+    Topic.
+
+get_admins() ->
+    Admins = chat_server:get_admins(),
+    io:format("Admin users: ~p~n", [Admins]),
+    Admins.
+
 loop(Username) ->
     receive
         {history, History} ->
             io:format("Received chat history: ~p~n", [History]),
+            loop(Username);
+        {offline, OffMsgs} ->
+            io:format("You have offline private messages: ~p~n", [OffMsgs]),
+            loop(Username);
+        {topic, NewTopic} ->
+            io:format("Chat topic is now: ~s~n", [NewTopic]),
             loop(Username);
         {chat, From, Message, Timestamp} ->
             io:format("[~p] ~s: ~s~n", [Timestamp, From, Message]),
@@ -102,9 +140,37 @@ loop(Username) ->
             loop(Username);
         disconnect ->
             chat_server:disconnect(Username),
-            io:format("Disconnected from chat server.~n"),
+            io:format("Disconnected from chat server.~n", []),
             unregister(chat_client),
             ok;
+        {set_topic, NewTopic} ->
+            Reply = chat_server:set_topic(Username, NewTopic),
+            io:format("Set topic reply: ~p~n", [Reply]),
+            loop(Username);
+        {kick, Target} ->
+            Reply = chat_server:kick(Username, Target),
+            io:format("Kick reply: ~p~n", [Reply]),
+            loop(Username);
+        {mute, Target, Duration} ->
+            Reply = chat_server:mute(Username, Target, Duration),
+            io:format("Mute reply: ~p~n", [Reply]),
+            loop(Username);
+        {unmute, Target} ->
+            Reply = chat_server:unmute(Username, Target),
+            io:format("Unmute reply: ~p~n", [Reply]),
+            loop(Username);
+        {promote, Target} ->
+            Reply = chat_server:promote(Username, Target),
+            io:format("Promote reply: ~p~n", [Reply]),
+            loop(Username);
+        {kick_notice, Target} ->
+            if Target =:= Username ->
+                io:format("You have been kicked from the chat.~n", []),
+                unregister(chat_client),
+                exit(normal);
+               true ->
+                loop(Username)
+            end;
         Other ->
             io:format("Unknown message: ~p~n", [Other]),
             loop(Username)
